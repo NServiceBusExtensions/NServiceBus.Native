5d6931ebe4099f5fbc076cbf1ebb3b83a8a05f23
Simon Cropp <simon.cropp@gmail.com>
2019-06-11 12:58:23 +1000

    Update DedupeIntegrationTests.cs

--- src/SqlServer.Native.Tests/DedupeIntegrationTests.cs
+++ src/SqlServer.Native.Tests/DedupeIntegrationTests.cs
@@ -17,20 +17,27 @@ public class DedupeIntegrationTests : TestBase
     [Fact]
     public async Task Integration()
     {
-        var endpoint = await StartEndpoint();
-        var messageId = Guid.NewGuid();
-        var result = await SendMessage(messageId, endpoint, "context1");
-        Assert.Equal("context1", result.Context);
-        Assert.Equal(DedupeOutcome.Sent, result.DedupeOutcome);
-        result = await SendMessage(messageId, endpoint, "context2");
-        Assert.Equal("context1", result.Context);
-        Assert.Equal(DedupeOutcome.Deduplicated, result.DedupeOutcome);
-        if (!countdown.Wait(TimeSpan.FromSeconds(20)))
-        {
-            throw new Exception("Expected dedup");
-        }
+        var database = await LocalDb();
 
-        await endpoint.Stop();
+        using (var connection = await database.OpenConnection())
+        {
+            var dedupeManager = new DedupeManager(connection, "Deduplication");
+            await dedupeManager.Create();
+            var endpoint = await StartEndpoint(connection.ConnectionString);
+            var messageId = Guid.NewGuid();
+            var result = await SendMessage(messageId, endpoint, "context1");
+            Assert.Equal("context1", result.Context);
+            Assert.Equal(DedupeOutcome.Sent, result.DedupeOutcome);
+            result = await SendMessage(messageId, endpoint, "context2");
+            Assert.Equal("context1", result.Context);
+            Assert.Equal(DedupeOutcome.Deduplicated, result.DedupeOutcome);
+            if (!countdown.Wait(TimeSpan.FromSeconds(20)))
+            {
+                throw new Exception("Expected dedup");
+            }
+
+            await endpoint.Stop();
+        }
     }
 
     static async Task<DedupeResult> SendMessage(Guid messageId, IEndpointInstance endpoint, string context)
@@ -45,22 +52,22 @@ public class DedupeIntegrationTests : TestBase
         return sendWithDedupe;
     }
 
-    static Task<IEndpointInstance> StartEndpoint()
+    static Task<IEndpointInstance> StartEndpoint(string connection)
     {
         var configuration = new EndpointConfiguration(nameof(DedupeIntegrationTests));
         configuration.UsePersistence<LearningPersistence>();
         configuration.EnableInstallers();
-        configuration.EnableDedupe(Connection.ConnectionString);
+        configuration.EnableDedupe(connection);
         configuration.PurgeOnStartup(true);
         configuration.UseSerialization<NewtonsoftSerializer>();
         configuration.DisableFeature<TimeoutManager>();
         configuration.DisableFeature<MessageDrivenSubscriptions>();
 
-        var attachments = configuration.EnableAttachments(Connection.ConnectionString, TimeToKeep.Default);
+        var attachments = configuration.EnableAttachments(connection, TimeToKeep.Default);
         attachments.UseTransportConnectivity();
 
         var transport = configuration.UseTransport<SqlServerTransport>();
-        transport.ConnectionString(Connection.ConnectionString);
+        transport.ConnectionString(connection);
         return Endpoint.Start(configuration);
     }
 
@@ -73,11 +80,9 @@ public class DedupeIntegrationTests : TestBase
         }
     }
 
-    public DedupeIntegrationTests(ITestOutputHelper output) : base(output)
+    public DedupeIntegrationTests(ITestOutputHelper output) :
+        base(output)
     {
-        var dedupeManager = new DedupeManager(SqlConnection, "Deduplication");
-        dedupeManager.Drop().Await();
-        dedupeManager.Create().Await();
     }
 
     class MyMessage : IMessage
